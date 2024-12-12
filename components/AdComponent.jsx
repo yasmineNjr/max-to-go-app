332e82c3d4f577c4fbe86162a38f75eb4339f6b2
yamineHjr <yasmine.alnajar@gmail.com>
2024-12-12 21:28:13 +0200

    finishing advertisement page

--- components/AdComponent.jsx
+++ components/AdComponent.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import CommandButton from './CommandButton'
-import Button from './Button'
 import Image from 'next/image'
+import ConfirmationModal from './ConfirmationModal'
 
 const AdComponent = ({ id, img}) => {
 
@@ -9,18 +9,19 @@ const AdComponent = ({ id, img}) => {
     <div key={id} 
          className="relative w-full h-96 lg:h-72 bg-gray-200 overflow-hidden border-2 border-secondaryColor rounded-xl"
     >
-        {/* Image */}
         <Image
         src={img}
         alt="Descriptive Alt Text" 
         className="absolute w-full h-full object-cover" 
         />
-        {/* Buttons */}
         <div className="absolute bottom-0 left-0 w-full bg-gradient-to-t from-black to-transparent text-center p-2 flex flex-wrap gap-3 justify-start bg-textSecondary h-fit">
             <CommandButton title='Delete'/>
             <CommandButton title='Amendment'/>
-            <CommandButton title='Pause a temporary display'/>
-            <Button title='Change the display duration'/>
+            <ConfirmationModal command={true} buttonText='Pause a temporary display' text='Do you really want to stop the show?'/>
+            <ConfirmationModal command={false} 
+                                buttonText='Change the display duration' 
+                                text='Change the display duration'
+                                form={true}/>
         </div>
     </div>
   )
--- components/ConfirmationModal.jsx
+++ components/ConfirmationModal.jsx
@@ -12,15 +12,17 @@ import {
 import { Button } from './ui/button'
 import Image from 'next/image'
 import { logo } from '@/public/assets'
+import { Input } from './ui/input'
 
-const ConfirmationModal = ({ buttonText, title, text}) => {
+const ConfirmationModal = ({ buttonText, form, text, command}) => {
 
     const [open, setOpen] = useState(false)
-
   return (
     <Dialog open={open} onOpenChange={setOpen}>
         <DialogTrigger asChild>
-        <Button variant='none' className={`w-[50%] flex flex-row items-center justify-center gap-2 py-4 px-6 bg-secondaryColor font-bold h-[25px] text-primaryColor outline-none rounded-xl`}>
+        <Button variant='none' className={command ? 
+                                `flex flex-row items-center justify-center gap-2 py-4 px-6 h-[25px] w-fit font-bold text-[14px] text-secondaryColor border border-secondaryColor outline-none rounded-xl ` : 
+                                `flex flex-row items-center justify-center gap-2 py-4 px-6 bg-secondaryColor font-bold h-[25px] text-primaryColor outline-none rounded-xl`}>
             {buttonText}
         </Button>
         </DialogTrigger>
@@ -31,12 +33,22 @@ const ConfirmationModal = ({ buttonText, title, text}) => {
                     <Image src={logo} alt='logo' width={250} height={250} className='ml-10'/>
                 </DialogTitle>
                 <DialogDescription className='text-center text-textDefault gap-3 text-[18px] flex flex-col items-center justify-center'>
-                {text}
-                <Button variant='none' 
-                        className={`mt-5 w-[50%] flex flex-row items-center justify-center gap-2 py-4 px-6 bg-secondaryColor font-bold h-[25px] text-primaryColor outline-none rounded-xl`}
-                        onClick={() => setOpen(false)}>
-                    OK
-                </Button>
+                    {text}
+                    {form && 
+                        <div className='rounded-2xl border-secondaryColor border-2'>
+                            <Input
+                                placeholder='duration'
+                                className="shad-input "
+                                type='number'
+                                min={1}
+                            />
+                        </div>
+                    }
+                    <Button variant='none' 
+                            className={`mt-5 w-[50%] flex flex-row items-center justify-center gap-2 py-4 px-6 bg-secondaryColor font-bold h-[25px] text-primaryColor outline-none rounded-xl`}
+                            onClick={() => setOpen(false)}>
+                        OK
+                    </Button>
                 </DialogDescription>
             </DialogHeader>
         </DialogContent>
