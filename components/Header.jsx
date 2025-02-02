9f3e037b68c9eaad922a32512c7f6741f26532d1
yamineHjr <yasmine.alnajar@gmail.com>
2025-02-02 22:22:48 +0200

    reorder header items

--- components/Header.jsx
+++ components/Header.jsx
@@ -25,28 +25,30 @@ const Header = () => {
     <div>
       {!isFirstPage && (
         <div className="w-full h-20 bg-secondary rounded-b-2xl flex flex-row items-center justify-between p-5 shadow-sm ">          
-          <div className="flex flex-row items-center justify-center gap-2 text-foreground">
+          <div className="flex flex-row items-center justify-center gap-1 md:gap-2 text-foreground">
+            <SidebarTrigger />
             <div 
                 className='w-9 h-9 text-foreground bg-transparent rounded-md p-2 transition-colors hover:bg-background border border-card focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 cursor-pointer'
                 onClick={() => router.push('/main')}>
               <House size={18}/>
             </div>
-            <SidebarTrigger />
-            <Image src={user} alt="user" className="h-[50px] w-[50px] rounded-full" />
-            <h3>Welcome Admin</h3>
-          </div>
-
-          <div className='flex flex-row items-center justify-center gap-2'>
-            <div className="flex items-center justify-end ml-5 rounded-xl bg-background text-foreground border border-card h-9 hover:bg-secondary  shadow-sm">
+            <div className="flex items-center justify-end rounded-xl bg-background text-foreground border border-card h-9 hover:bg-secondary  shadow-sm">
               <Search className='m-2'/>
               <Input
                 placeholder="Search..."
-                className="shad-input"
+                className="shad-input w-32 md:w-36"
                 value={searchQuery}
                 onChange={handleSearch}
               />
             </div>
+          </div>
+
+          <div className='flex flex-row items-center justify-center gap-1 md:gap-2'>   
             <ModeToggle/>
+            <div className='flex flex-col'>
+              <Image src={user} alt="user" className="h-[45px] w-[45px] rounded-full" />
+              {/* <span className='hidden md:block text-sm font-medium'>Admin</span> */}
+            </div>
           </div>
         </div>
       )}
